4c3eee0de659899d8b8ee6d2b06f608cf8b057bc
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-03 18:49:07 +0530

    [keerthana]Added::switch case

--- Employee Computation/Employee Computation/Program.cs
+++ Employee Computation/Employee Computation/Program.cs
@@ -7,20 +7,22 @@ namespace Employee_Computation
         static void Main(string[] args)
         {
             Console.WriteLine("calculation of employee computation");
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
+                case 2:
+                    empHrs = 4;
+                    break;
+                case 1:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage :" + empWage);
