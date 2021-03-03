fa5be7d15cbe2cbb5399cfa8fb8cdd2cd1cb1d00
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-03 18:01:32 +0530

    [keerthana]Added::employee present or absent

--- Employee Computation/Employee Computation/Program.cs
+++ Employee Computation/Employee Computation/Program.cs
@@ -7,6 +7,14 @@ namespace Employee_Computation
         static void Main(string[] args)
         {
             Console.WriteLine("calculation of employee computation");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present=" + empCheck);
+            else
+                Console.WriteLine("Employee is absent=" + empCheck);
+            Console.ReadKey();
         }
     }
 }
