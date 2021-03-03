a392365370d00bbf03260651368a1b1e45acf8a8
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-03 17:42:17 +0530

    [keerthana]Added::welcome

---
+++ Employee Computation/Employee Computation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Computation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calculation of employee computation");
+        }
+    }
+}
