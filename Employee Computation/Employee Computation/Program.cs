4beed562deaf105ad04fe678bcbf6c4669040f24
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-03 19:04:34 +0530

    [keerthana]Added::emlpyee wage

--- Employee Computation/Employee Computation/Program.cs
+++ Employee Computation/Employee Computation/Program.cs
@@ -4,28 +4,36 @@ namespace Employee_Computation
 {
     class Program
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 50;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-            Console.WriteLine("calculation of employee computation");
-            int EMP_RATE_PER_HOUR = 20;
+            Console.WriteLine("Welcome to Employee Wage Computation");
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int temp = 0; temp < NUM_OF_WORKING_DAYS; temp++)
             {
-                case 2:
-                    empHrs = 4;
-                    break;
-                case 1:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 2:
+                        empHrs = 4;
+                        break;
+                    case 1:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage :" + empWage);
+            Console.WriteLine("Total Monthly Emp Wage : " + totalEmpWage);
         }
     }
 }
