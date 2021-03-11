6e2b54d93ceb7a84ea0c04c3c09c61426cf54810
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-11 12:28:50 +0530

    Added::employee wage method

--- Employee Computation/Employee Computation/Program.cs
+++ Employee Computation/Employee Computation/Program.cs
@@ -6,16 +6,14 @@ namespace Employee_Computation
     {
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 50;
-        public const int NUMBER_OF_WORKING_DAYS = 22;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUMBER_OF_WORKING_DAYS = 20;
         public const int MAX_HRS_IN_MONTH = 100;
-        static void Main(string[] args)
+
+        public static int calculateEmployee()
         {
-            Console.WriteLine("Welcome to Employee Wage Computation");
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUMBER_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -35,8 +33,16 @@ namespace Employee_Computation
                 totalEmpHrs += empHrs;
             }
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Monthly Employee Wage is : " + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(String[] args)
+        {
+
+            Console.WriteLine($"Total Employee Wage is : {calculateEmployee()}");
+
         }
     }
-} 
+}
+
+
 
